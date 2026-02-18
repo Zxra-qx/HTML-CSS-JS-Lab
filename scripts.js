9a429ef6156a3e1259912d5a7ba58aabf5a36c8b
Zxra-qx <zaraqad@gmail.com>
2026-02-17 17:04:04 -0700

    Add model and duration selection functionality

---
+++ scripts.js
@@ -0,0 +1,57 @@
+let modelName = "XYZ";
+let duration = 0;
+
+function recalculate() {
+    let costLabel = document.getElementById("calculated-cost");
+
+    let totalCost = 0;
+
+    if (modelName === "XYZ") {
+        totalCost = duration * 100;
+    } else if (modelName === "CPRG") {
+        totalCost = duration * 213;
+    }
+
+    costLabel.innerHTML = totalCost.toFixed(2);
+}
+
+let modelButton = document.getElementById("model-button");
+
+
+function changeModel() {
+    let modelText = document.getElementById("model-text");
+
+    if (modelName === "XYZ") {
+        modelName = "CPRG";
+        modelText.innerHTML = "Model CPRG";
+    } else {
+        modelName = "XYZ";
+        modelText.innerHTML = "Model XYZ";
+    }
+
+    recalculate();
+}
+
+modelButton.addEventListener("click", changeModel);
+
+
+
+let durationButton = document.getElementById("duration-button");
+
+function changeDuration() {
+    let durationText = document.getElementById("duration-text");
+
+    let newDuration = prompt("Enter number of days to book:");
+
+    // convert to number
+    duration = Number(newDuration);
+
+    // update HTML
+    durationText.innerHTML = duration;
+
+    // recalc cost
+    recalculate();
+}
+
+// attach event listener
+durationButton.addEventListener("click", changeDuration);
